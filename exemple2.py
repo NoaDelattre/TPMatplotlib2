bb13317ebc763dfddc24e99c95b87a7be176a769
NoaDelattre <lamarcknoadelattre@gmail.com>
2025-11-17 14:17:31 +0100

    v1.2 langlace

---
+++ exemple2.py
@@ -0,0 +1,5 @@
+from pylab import *
+x=2;y=3;plot(x,y,'*r')
+x2=4;y2=5;plot(x2,y2,'*r')
+x3=5;y3=8;plot(x3,y3,'*r')
+show()
---
+++ exemple3.py
@@ -0,0 +1,5 @@
+from pylab import *
+x=[2,4,5]
+y=[3,5,8]
+plot(x,y)
+show()
